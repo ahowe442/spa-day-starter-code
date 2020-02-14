3d9ae4b60924059569957db2a1d449ea5c1bba81
@ahowe442 <howe.abigail@gmail.com>
2020-02-13 19:45:40 -0600

    verifies passwords, adds attributes in processAddUserForm

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,9 +1,37 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm(){
+        return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify){
+        //Verify that passwords match.
+        if (user.getPassword().equals(verify)){
+            return "user/add";
+        }
+        else {
+            model.addAttribute("user", user);
+            model.addAttribute("verify", verify);
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            model.addAttribute("error", "Passwords do not match.");
+            return "user/add";
+        }
+    }
+
+
 }
